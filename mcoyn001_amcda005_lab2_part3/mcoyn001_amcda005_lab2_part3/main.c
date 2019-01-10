83cf5fff202576c6957799aa60e93acf261e3827
ASHTM15 <amcda005@ucr.edu>
2019-01-10 13:50:23 -0800

    made part3

---
+++ mcoyn001_amcda005_lab2_part3/mcoyn001_amcda005_lab2_part3/main.c
@@ -0,0 +1,18 @@
+/*
+ * mcoyn001_amcda005_lab2_part3.c
+ *
+ * Created: 1/10/2019 1:21:12 PM
+ * Author : ashle
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
