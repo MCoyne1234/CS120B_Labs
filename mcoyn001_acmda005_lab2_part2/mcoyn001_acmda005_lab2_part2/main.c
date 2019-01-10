bd7f20beccc403d3bed4057c155a773fb13d85ee
MCoyne1234 <32471492+MCoyne1234@users.noreply.github.com>
2019-01-10 13:18:31 -0800

    lab2 part 2 and fixed file headers.

--- mcoyn001_acmda005_lab2_part2/mcoyn001_acmda005_lab2_part2/main.c
+++ mcoyn001_acmda005_lab2_part2/mcoyn001_acmda005_lab2_part2/main.c
@@ -1,22 +1,23 @@
 /*    Partner(s) Name & E-mail:Ashley McDaniel amcda005@ucr.edu & Matthew Coyne mcoyn001@ucr.edu
  *    Lab Section: 022
- *    Assignment: Lab # 2 Exercise # 1
+ *    Assignment: Lab # 2 Exercise # 2
  *    Exercise Description: [optional - include for your own benefit]
  *      The program will assign A input value to B and so on.
  *    I acknowledge all content contained herein, excluding template or example
  *    code, is my own original work.
  */
 
-#include <avr/io.h>
-
 int main(void)
 {
-	DDRB = 0xFF; // Configure port B's 8 pins as outputs
-	PORTB = 0x00; // Initialize PORTB output to 0’s
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+	// Initialize output on PORTB to 0x00
+	unsigned char temp_input = 0x00;
 	while(1)
 	{
-		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+		temp_input = PINA;
+		PORTB = temp_input;    // Writes port B's 8 pins with the values
+		// on port A's 8 pins
 	}
-}
 
 
