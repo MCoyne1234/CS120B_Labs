69594836708dd9c6c4bbb88ea9830bdf0e9002a9
ASHTM15 <amcda005@ucr.edu>
2019-02-13 18:03:18 -0800

    testerforbuzzer

---
+++ testBuzzer/testBuzzer/main.c
@@ -0,0 +1,38 @@
+/*
+ * testBuzzer.c
+ *
+ * Created: 2/13/2019 1:28:52 PM
+ * Author : ashle
+ */ 
+
+#include <avr/io.h>
+#include "timer.h"
+
+int main(void)
+{
+	DDRB = 0xFF; // init
+	DDRA = 0x00; 
+	unsigned char temp_a = 0x00; 
+	int count = 0; //buzzer count for 2 ms
+	
+	TimerSet(2); //set timer to 1 ms
+	TimerOn();   //turn timer on
+	TimerFlag = 0;    
+	
+    while (1) 
+    {
+		temp_a = (PINA & 0x04); 
+		if(temp_a && (count < 2)){
+			PORTB = (PORTB^0xFF); 
+			count++; //increment count until 2 
+		}
+		else{
+			count = 0; //reset counter
+			PORTB = 0x00; 
+		}
+		while(!TimerFlag){}
+		TimerFlag = 0; 
+		temp_a = 0; 
+    }
+}
+
