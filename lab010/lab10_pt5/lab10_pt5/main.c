4c109b862fd775f3729938c1d6c33c921c418908
MCoyne1234 <32471492+MCoyne1234@users.noreply.github.com>
2019-02-16 00:30:25 -0800

    Initi part 5

---
+++ lab010/lab10_pt5/lab10_pt5/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab10_pt5.c
+ *
+ * Created: 2/16/2019 12:29:35 AM
+ * Author : Administrator
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
